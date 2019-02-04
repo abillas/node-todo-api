8f703263934399f45e9df2e230ca7c18f313021a
abillas <47216690+abillas@users.noreply.github.com>
2019-02-04 16:28:12 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ app.post("/todos", (req, res) => {
   var todo = new Todo({
     text: req.body.text
   });
+
   todo.save().then(
     doc => {
       res.send(doc);
@@ -23,6 +24,21 @@ app.post("/todos", (req, res) => {
   );
 });
 
-app.listen(8888, () => {
-  console.log("Started in port 8888!");
+app.get("/todos", (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({
+        todos
+      });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
 });
+
+app.listen(3000, () => {
+  console.log("Started on port 3000");
+});
+
+module.exports = { app };
