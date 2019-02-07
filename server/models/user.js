ecab56f3303aee47cf35cf5d40ff33f980360d65
abillas <47216690+abillas@users.noreply.github.com>
2019-02-07 17:40:12 +0100

    Add Get / users/me and authentication|

--- server/models/user.js
+++ server/models/user.js
@@ -12,7 +12,7 @@ var UserSchema = new mongoose.Schema({
     unique: true,
     validate: {
       validator: validator.isEmail,
-      message: "{VALUE} not a valid email."
+      message: "{VALUE} is not a valid email"
     }
   },
   password: {
@@ -48,10 +48,27 @@ UserSchema.methods.generateAuthToken = function() {
     .sign({ _id: user._id.toHexString(), access }, "abc123")
     .toString();
 
-  user.tokens = user.tokens.concat([{ access, token }]);
+  user.tokens.push({ access, token });
 
   return user.save().then(() => {
     return token;
+  });
+};
+
+UserSchema.statics.findByToken = function(token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, "abc123");
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    _id: decoded._id,
+    "tokens.token": token,
+    "tokens.access": "auth"
   });
 };
 
--- server/server.js
+++ server/server.js
@@ -8,9 +8,10 @@ const { ObjectID } = require("mongodb");
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
+var { authenticate } = require("./middleware/authenticate");
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -108,6 +109,7 @@ app.patch("/todos/:id", (req, res) => {
     });
 });
 
+// POST /users
 app.post("/users", (req, res) => {
   var body = _.pick(req.body, ["email", "password"]);
   var user = new User(body);
@@ -121,8 +123,12 @@ app.post("/users", (req, res) => {
       res.header("x-auth", token).send(user);
     })
     .catch(e => {
-      res.status(400).send();
+      res.status(400).send(e);
     });
+});
+
+app.get("/users/me", authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
