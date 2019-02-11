bc6309a183a47d4f13dcd758343d23f5921a1c62
abillas <47216690+abillas@users.noreply.github.com>
2019-02-11 11:03:50 +0100

    Changes

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -15,7 +15,9 @@ const users = [
     tokens: [
       {
         access: "auth",
-        token: jwt.sign({ _id: userOneID, access: "auth" }, "abc123").toString()
+        token: jwt
+          .sign({ _id: userOneID, access: "auth" }, process.env.JWT_SECRET)
+          .toString()
       }
     ]
   },
@@ -26,7 +28,9 @@ const users = [
     tokens: [
       {
         access: "auth",
-        token: jwt.sign({ _id: userTwoID, access: "auth" }, "abc123").toString()
+        token: jwt
+          .sign({ _id: userTwoID, access: "auth" }, process.env.JWT_SECRET)
+          .toString()
       }
     ]
   }
